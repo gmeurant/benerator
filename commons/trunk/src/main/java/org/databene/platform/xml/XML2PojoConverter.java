4a18f3b9087c8ca302cba09c78f71057dfca9c1b
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-02-02 08:55:35 +0000

    Initial version

---
+++ commons/trunk/src/main/java/org/databene/platform/xml/XML2PojoConverter.java
@@ -0,0 +1,76 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.platform.xml;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.databene.commons.BeanUtil;
+import org.databene.commons.Converter;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+
+/**
+ * Maps element names to JavaBean class names and attributes to JavaBean properties.<br/>
+ * <br/>
+ * Created: 29.01.2008 14:35:40
+ * @since 0.3.0
+ * @author Volker Bergmann
+ */
+public class XML2PojoConverter implements Converter<Element, Object> {
+
+    // TODO v0.3.1 test
+    private Map<String, Class<? extends Object>> types;
+    
+    public XML2PojoConverter() {
+        this.types = new HashMap<String, Class<? extends Object>>();
+    }
+    
+    public void addMapping(String name, Class<? extends Object> type) {
+        types.put(name, type);
+    }
+
+    public Class<Object> getTargetType() {
+        return Object.class;
+    }
+
+    public Object convert(Element element) {
+        String elementName = element.getNodeName();
+        Class<? extends Object> type = types.get(elementName);
+        Object bean = BeanUtil.newInstance(type);
+        NamedNodeMap attributes = element.getAttributes();
+        for (int i = 0; i < attributes.getLength(); i++) {
+            Attr attribute = (Attr) attributes.item(i);
+            String propertyName = attribute.getName();
+            String propertyValue = attribute.getValue();
+            BeanUtil.setPropertyValue(bean, propertyName, propertyValue);
+        }
+        return bean;
+    }
+
+}
