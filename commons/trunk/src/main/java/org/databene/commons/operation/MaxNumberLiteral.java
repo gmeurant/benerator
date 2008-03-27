439f9f5a90338a49d79e536b533615bc8cff2aed
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-03-27 07:37:06 +0000

    Initial version

---
+++ commons/trunk/src/main/java/org/databene/commons/operation/MaxNumberLiteral.java
@@ -0,0 +1,45 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package org.databene.commons.operation;
+
+import java.math.BigDecimal;
+
+import org.databene.commons.converter.String2NumberConverter;
+
+/**
+ * Returns the maximum value of several number literals.<br/><br/>
+ * Created: 08.03.2008 07:18:09
+ * @since 0.4.0
+ * @author Volker Bergmann
+ */
+public class MaxNumberLiteral extends ConvertingOperation<String, BigDecimal> {
+
+    public MaxNumberLiteral() {
+        super(new String2NumberConverter<BigDecimal>(BigDecimal.class), new MaxOperation<BigDecimal>());
+    }
+}
