3a2f3dfec427f76564d71ac033fe501ea338931b
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-03-27 06:10:11 +0000

    Initial version

---
+++ commons/trunk/src/main/java/org/databene/commons/MathUtil.java
@@ -0,0 +1,63 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.databene.commons;
+
+/**
+ * Provides mathematical utility methods.
+ * @since 0.4.0
+ * @author Volker Bergmann
+ */
+public class MathUtil {
+    
+    /**
+     * Returns the number of digits needed for displaying the postfix values of a number, but at most 7.
+     * @param number
+     * @return
+     */
+    public static int fractionDigits(double number) {
+        double x = fraction(number);
+        int n = 0;
+        while (x >= 0.0000001 && n < 7) {
+            n++;
+            x = fraction(x * 10);
+        }
+        return n;
+    }
+
+    private static double fraction(double number) {
+        double value = Math.IEEEremainder(Math.abs(number), 1);
+        if (value < 0)
+            value += 1;
+        return value;
+    }
+    
+    public static int prefixDigits(double number) {
+        number = Math.abs(number);
+        if (number <= 1)
+            return 1;
+        return 1 + (int) Math.log10(number);
+    }
+}
