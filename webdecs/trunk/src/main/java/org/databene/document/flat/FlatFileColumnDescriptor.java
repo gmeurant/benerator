d18075335baf65a63d7bb063d89a0168b04a7d5d
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-04-29 18:02:31 +0000

    Added toString() equals() and hashcode()

--- webdecs/trunk/src/main/java/org/databene/document/flat/FlatFileColumnDescriptor.java
+++ webdecs/trunk/src/main/java/org/databene/document/flat/FlatFileColumnDescriptor.java
@@ -75,4 +75,40 @@ public class FlatFileColumnDescriptor {
     public PadFormat getFormat() {
         return format;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((format == null) ? 0 : format.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final FlatFileColumnDescriptor other = (FlatFileColumnDescriptor) obj;
+		if (format == null) {
+			if (other.format != null)
+				return false;
+		} else if (!format.equals(other.format))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+    
+    @Override
+    public String toString() {
+    	return name + '[' + format.getLength() + format.getAlignment().getId() + format.getPadChar() + ']';
+    }
 }
