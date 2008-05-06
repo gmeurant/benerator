be2ce6c4ce32c7fbc674a6a13e15f1f9796f5f98
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-05-06 07:51:27 +0000

    Initial version

---
+++ commons/trunk/src/test/java/org/databene/commons/DBUtilTest.java
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.commons;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import org.databene.commons.db.DBUtil;
+import org.databene.commons.db.hsql.HSQLUtil;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the {@link DBUtil} class.<br/><br/>
+ * Created at 03.05.2008 15:53:49
+ * @since 0.5.3
+ * @author Volker Bergmann
+ *
+ */
+public class DBUtilTest extends TestCase {
+
+	public void testRunScript() throws SQLException, IOException {
+		Connection connection = HSQLUtil.connectInMemoryDB(getClass().getSimpleName());
+		DBUtil.runScript("org/databene/commons/create_tables.hsql.sql", connection, true, false);
+		String[][] rows = DBUtil.query("select * from T1;", connection);
+		assertEquals(1, rows.length);
+		assertTrue(Arrays.equals(ArrayUtil.toArray("1", "R&B"), rows[0]));
+		System.out.println(rows);
+	}
+}
