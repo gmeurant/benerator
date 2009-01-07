70bc59023fa3c3a0b8684f1366bcd67a63ac2fb2
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-01-07 07:41:54 +0000

    Added generic type

--- commons/trunk/src/main/java/org/databene/commons/validator/AbstractValidator.java
+++ commons/trunk/src/main/java/org/databene/commons/validator/AbstractValidator.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2008-2009 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -39,6 +39,6 @@ import org.databene.commons.Validator;
  * @since 0.4.6
  * @author Volker Bergmann
  */
-public abstract class AbstractValidator implements Validator {
+public abstract class AbstractValidator<E> implements Validator<E> {
 
 }
