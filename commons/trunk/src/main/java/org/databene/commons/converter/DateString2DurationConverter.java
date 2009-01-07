faa10d3290ee9a3a1461c744894d6873245317f2
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-01-07 07:40:05 +0000

    Initial version

---
+++ commons/trunk/src/main/java/org/databene/commons/converter/DateString2DurationConverter.java
@@ -0,0 +1,62 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.commons.converter;
+
+import java.util.Date;
+
+import org.databene.commons.ConversionException;
+
+/**
+ * Converts a date in String format to a duration in milliseconds. This takes the dates 1970-01-01 or 0000-00-00 as base.<br/>
+ * <br/>
+ * Created at 05.01.2009 18:37:23
+ * @since 0.4.7
+ * @author Volker Bergmann
+ */
+
+public class DateString2DurationConverter extends AbstractConverter<String, Long> {
+	
+	private static ConverterChain<String, Long> helper = new ConverterChain<String, Long>(
+			new String2DateConverter<Date>(),
+			new Date2LongConverter()
+		);
+
+	public DateString2DurationConverter() {
+		super(Long.class);
+	}
+
+	@Override
+	public Long convert(String sourceValue) throws ConversionException {
+		if (sourceValue == null)
+			return null;
+		Long result = helper.convert(sourceValue);
+		if (result < 0)
+			result += 62170156800000L;
+		return result;
+	}
+
+}
