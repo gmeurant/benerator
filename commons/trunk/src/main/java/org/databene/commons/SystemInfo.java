cc57873aeb5f052d061b03cdb9cba1735ec731dd
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-03-15 11:40:27 +0000

    Changed interface

--- commons/trunk/src/main/java/org/databene/commons/SystemInfo.java
+++ commons/trunk/src/main/java/org/databene/commons/SystemInfo.java
@@ -52,89 +52,195 @@ public final class SystemInfo {
 	private static final String MAC_OS_X = "Mac OS X";
 	private static final String WINDOWS = "Windows";
 
-	// TODO v0.5.8 create getters and setters for each System property and deprecate the old methods
-	
+	/**
+     * @return the OS name
+     * @deprecated use {@link #getOsName()}
+     */
+	@Deprecated
+    public static String osName() {
+        return System.getProperty(OS_NAME_KEY);
+    }
+
 	/**
      * @return the OS name
      */
-    public static String osName() {
+    public static String getOsName() {
         return System.getProperty(OS_NAME_KEY);
     }
 
     /**
      * @return the OS architecture
+     * @deprecated use {@link #getOsArchitecture()}
      */
+	@Deprecated
     public static String osArchitecture() {
         return System.getProperty(OS_ARCH_KEY);
     }
 
     /**
-     * @return the OS version
+     * @return the OS architecture
      */
+    public static String getOsArchitecture() {
+        return System.getProperty(OS_ARCH_KEY);
+    }
+
+    /**
+     * @return the OS version
+     * @deprecated use {@link #getOsVersion()}
+     */
+	@Deprecated
     public static String osVersion() {
         return System.getProperty(OS_VERSION_KEY);
     }
 
     /**
-     * @return Line separator ("\n" on UNIX)
+     * @return the OS version
      */
+    public static String getOsVersion() {
+        return System.getProperty(OS_VERSION_KEY);
+    }
+
+    /**
+     * @return Line separator ("\n" on UNIX)
+     * @deprecated use {@link #getLineSeparator()}
+     */
+	@Deprecated
     public static String lineSeparator() {
         return System.getProperty(LINE_SEPARATOR_KEY);
     }
 
     /**
-     * @return Path separator (":" on UNIX)
+     * @return Line separator ("\n" on UNIX)
      */
+    public static String getLineSeparator() {
+        return System.getProperty(LINE_SEPARATOR_KEY);
+    }
+
+    /**
+     * @return Path separator (":" on UNIX)
+     * @deprecated use {@link #getPathSeparator()}
+     */
+	@Deprecated
     public static String pathSeparator() {
         return System.getProperty(PATH_SEPARATOR_KEY);
     }
 
     /**
-     * @return File separator ("/" on UNIX)
+     * @return Path separator (":" on UNIX)
      */
+    public static String getPathSeparator() {
+        return System.getProperty(PATH_SEPARATOR_KEY);
+    }
+
+    /**
+     * @return File separator ("/" on UNIX)
+     * @deprecated use {@link #getFileSeparator()}
+     */
+	@Deprecated
     public static char fileSeparator() {
         return System.getProperty(FILE_SEPARATOR_KEY).charAt(0);
     }
 
     /**
-     * @return the user's current directory
+     * @return File separator ("/" on UNIX)
      */
+    public static char getFileSeparator() {
+        return System.getProperty(FILE_SEPARATOR_KEY).charAt(0);
+    }
+
+    /**
+     * @return the user's current directory
+     * @deprecated use {@link #getCurrentDir()}
+     */
+	@Deprecated
     public static String currentDir() {
         return System.getProperty(USER_DIR_KEY);
     }
 
     /**
-     * @return the user's name
+     * @return the user's current directory
      */
+    public static String getCurrentDir() {
+        return System.getProperty(USER_DIR_KEY);
+    }
+
+    /**
+     * @return the user's name
+     * @deprecated use {@link #getUserName()}
+     */
+	@Deprecated
     public static String userName() {
         return System.getProperty(USER_NAME_KEY);
     }
 
     /**
-     * @return the user's home directory
+     * @return the user's name
      */
+    public static String getUserName() {
+        return System.getProperty(USER_NAME_KEY);
+    }
+
+    /**
+     * @return the user's home directory
+     * @deprecated use {@link #getUserHome()}
+     */
+	@Deprecated
     public static String userHome() {
         return System.getProperty(USER_HOME_KEY);
     }
 
     /**
-     * @return the default temp file path
+     * @return the user's home directory
      */
+    public static String getUserHome() {
+        return System.getProperty(USER_HOME_KEY);
+    }
+
+    /**
+     * @return the default temp file path
+     * @deprecated use {@link #getTempDir()}
+     */
+	@Deprecated
     public static String tempDir() {
         return System.getProperty(JAVA_IO_TMPDIR_KEY);
     }
 
     /**
-     * @return the file encoding
+     * @return the default temp file path
      */
+    public static String getTempDir() {
+        return System.getProperty(JAVA_IO_TMPDIR_KEY);
+    }
+
+    /**
+     * @return the file encoding
+     * @deprecated use {@link #getFileEncoding()}
+     */
+	@Deprecated
     public static String fileEncoding() {
         return System.getProperty(FILE_ENCODING_KEY);
     }
 
     /**
+     * @return the file encoding
+     */
+    public static String getFileEncoding() {
+        return System.getProperty(FILE_ENCODING_KEY);
+    }
+
+    /**
+     * @return user language
+     * @deprecated use {@link #getUserLanguage()}
+     */
+	@Deprecated
+    public static String userLanguage() {
+        return System.getProperty(USER_LANGUAGE_KEY);
+    }
+
+    /**
      * @return user language
      */
-    public static String userLanguage() {
+    public static String getUserLanguage() {
         return System.getProperty(USER_LANGUAGE_KEY);
     }
 
@@ -142,21 +248,29 @@ public final class SystemInfo {
 	 * @return true if the system is a Windows version, else false
 	 */
 	public static boolean isWindows() {
-		return osName().startsWith(WINDOWS);
+		return getOsName().startsWith(WINDOWS);
 	}
 
 	/**
 	 * @return true if the system is Mac OS X, else false
 	 */
 	public static boolean isMacOsx() {
-		return osName().startsWith(MAC_OS_X);
+		return getOsName().startsWith(MAC_OS_X);
 	}
 
 	/**
 	 * @return the system's default {@link Charset}
 	 */
+	/*
 	public static Charset charset() {
-		return Charset.forName(fileEncoding());
+		return Charset.forName(getFileEncoding());
+	}
+*/
+	/**
+	 * @return the system's default {@link Charset}
+	 */
+	public static Charset getCharset() {
+		return Charset.forName(getFileEncoding());
 	}
 
 }
