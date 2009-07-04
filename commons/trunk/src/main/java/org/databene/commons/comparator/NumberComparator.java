22acb396b401f5e2b898e58b6db48fe18ce14b7c
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-07-04 07:54:37 +0000

    made comparation available in static method

--- commons/trunk/src/main/java/org/databene/commons/comparator/NumberComparator.java
+++ commons/trunk/src/main/java/org/databene/commons/comparator/NumberComparator.java
@@ -34,29 +34,34 @@ import java.math.BigDecimal;
  * Generic comparator for Number objects of different type.<br/>
  * <br/>
  * Created: 09.10.2006 19:46:22
+ * @since 0.1
  * @author Volker Bergmann
  */
-public class NumberComparator<T extends Number> implements Comparator<T> {
+public class NumberComparator<E extends Number> implements Comparator<E> {
 
-    public int compare(T n1, T n2) {
-        if (n1 == null || n2 == null)
+    public int compare(E n1, E n2) {
+        return compareNumbers(n1, n2);
+    }
+
+	public static <T extends Number> int compareNumbers(T n1, T n2) {
+	    if (n1 == null || n2 == null)
             throw new IllegalArgumentException("comparing null value");
         if (n1 instanceof Integer)
-            return ((Integer)n1).compareTo(n2.intValue());
+            return ((Integer) n1).compareTo(n2.intValue());
         else if (n1 instanceof Long)
-            return ((Long)n1).compareTo(n2.longValue());
+            return ((Long) n1).compareTo(n2.longValue());
         else if (n1 instanceof Short)
-            return ((Short)n1).compareTo(n2.shortValue());
+            return ((Short) n1).compareTo(n2.shortValue());
         else if (n1 instanceof Byte)
-            return ((Byte)n1).compareTo(n2.byteValue());
+            return ((Byte) n1).compareTo(n2.byteValue());
         else if (n1 instanceof Float)
-            return ((Float)n1).compareTo(n2.floatValue());
+            return ((Float) n1).compareTo(n2.floatValue());
         else if (n1 instanceof Double)
-            return ((Double)n1).compareTo(n2.doubleValue());
+            return ((Double) n1).compareTo(n2.doubleValue());
         else if (n1 instanceof BigInteger)
-            return ((BigInteger)n1).compareTo((BigInteger)n2);
+            return ((BigInteger) n1).compareTo((BigInteger) n2);
         else if (n1 instanceof BigDecimal)
-            return ((BigDecimal)n1).compareTo((BigDecimal)n2);
+            return ((BigDecimal) n1).compareTo((BigDecimal) n2);
         else
             throw new UnsupportedOperationException("Unsupported Number type: " + n1.getClass());
     }
