2b25b785d8044904fa2a67f73a8aee012ca517c0
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-07-24 20:57:09 +0000

    Initial version

---
+++ commons/trunk/src/main/java/org/databene/commons/converter/String2GregorianCalendarConverter.java
@@ -0,0 +1,51 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.commons.converter;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import org.databene.commons.ConversionException;
+
+/**
+ * Parses a {@link String} as a {@link Calendar}.<br/>
+ * <br/>
+ * Created at 13.07.2009 18:49:00
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+
+public class String2GregorianCalendarConverter extends FixedSourceTypeConverter<String, GregorianCalendar> {
+
+    public String2GregorianCalendarConverter() {
+	    super(String.class, GregorianCalendar.class);
+    }
+
+    public GregorianCalendar convert(String sourceValue) throws ConversionException {
+	    throw new UnsupportedOperationException("Not implemented yet"); // TODO implement
+    }
+
+}
