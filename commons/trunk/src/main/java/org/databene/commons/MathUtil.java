fa091c97a2b08b4bf0850258b2580ae1d3b06a74
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-08-03 10:35:05 +0000

    new methods: - weightedSumOfSumOfDigits(), weightedSumOfDigits() - toByte(), toShort(), toInteger(), toLong(), - toFloat(), toDouble(), toBigDecimal(), toBigInteger()

--- commons/trunk/src/main/java/org/databene/commons/MathUtil.java
+++ commons/trunk/src/main/java/org/databene/commons/MathUtil.java
@@ -25,6 +25,11 @@
  */
 package org.databene.commons;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.databene.commons.converter.NumberConverter;
+
 /**
  * Provides mathematical utility methods.
  * @since 0.4.0
@@ -71,11 +76,51 @@ public class MathUtil {
 		return result;
 	}
 	
-	public static int weightedSumOfDigits(String number, int startIndex, int... weights) {
+	public static int weightedSumOfSumOfDigits(String number, int startIndex, int... weights) {
 	    int sum = 0;
 	    for (int i = 0; i < weights.length; i++)
 	    	sum += MathUtil.sumOfDigits(weights[i] * (number.charAt(startIndex + i) - '0'));
 	    return sum;
     }
+
+	public static int weightedSumOfDigits(String number, int startIndex, int... weights) {
+	    int sum = 0;
+	    for (int i = 0; i < weights.length; i++)
+	    	sum += weights[i] * (number.charAt(startIndex + i) - '0');
+	    return sum;
+    }
+
+	public static <T extends Number> Byte toByte(T value) {
+	    return NumberConverter.convert(value, Byte.class);
+    }
+	
+	public static <T extends Number> Short toShort(T value) {
+	    return NumberConverter.convert(value, Short.class);
+    }
+	
+	public static <T extends Number> Integer toInteger(T value) {
+	    return NumberConverter.convert(value, Integer.class);
+    }
+	
+	public static <T extends Number> Long toLong(T value) {
+	    return NumberConverter.convert(value, Long.class);
+    }
+	
+	public static <T extends Number> Float toFloat(T value) {
+	    return NumberConverter.convert(value, Float.class);
+    }
+	
+	public static <T extends Number> Double toDouble(T value) {
+	    return NumberConverter.convert(value, Double.class);
+    }
+	
+	public static <T extends Number> BigDecimal toBigDecimal(T value) {
+	    return NumberConverter.convert(value, BigDecimal.class);
+    }
+	
+	public static <T extends Number> BigInteger toBigInteger(T value) {
+	    return NumberConverter.convert(value, BigInteger.class);
+    }
+	
 
 }
