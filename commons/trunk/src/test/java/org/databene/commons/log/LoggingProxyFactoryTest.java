d951084d0eb4267dd239b0577c3cec2b26499a1c
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-09-20 17:02:41 +0000

    Initial version

---
+++ commons/trunk/src/test/java/org/databene/commons/log/LoggingProxyFactoryTest.java
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.commons.log;
+
+import junit.framework.TestCase;
+
+/**
+ * Creates a proxy to a class which logs all invocations of parent interface methods.<br/>
+ * <br/>
+ * Created at 17.09.2009 18:47:09
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+
+public class LoggingProxyFactoryTest extends TestCase {
+
+	public void test() {
+		Calc proxy = LoggingProxyFactory.createProxy(Calc.class, new CalcImpl());
+		assertEquals(3, proxy.add(1, 2));
+	}
+	
+	public static interface Calc {
+		public int add(int a, int b);
+	}
+	
+	public static class CalcImpl implements Calc {
+        public int add(int a, int b) {
+	        return a + b;
+        }
+	}
+	
+}
