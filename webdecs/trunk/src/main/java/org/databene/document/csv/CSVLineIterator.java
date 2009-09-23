05cdf8401cb261f75be03dd9e15f825b6c120d1e
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-09-23 11:28:25 +0000

    Javadoc

--- webdecs/trunk/src/main/java/org/databene/document/csv/CSVLineIterator.java
+++ webdecs/trunk/src/main/java/org/databene/document/csv/CSVLineIterator.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2007 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2007-2009 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -39,6 +39,7 @@ import java.util.ArrayList;
  * Gives accesses to content of a CSV file by String arrays
  * that represent the CSV rows as specified in RFC 4180.<br/>
  * <br/>
+ * @author Volker Bergmann
  */
 public class CSVLineIterator implements HeavyweightIterator<String[]> {
 
