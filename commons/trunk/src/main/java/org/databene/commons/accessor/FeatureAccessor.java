e3930572fbf0eb60827cc67ef709aea1fe8ba82c
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-10-10 08:22:31 +0000

    Added attribute access

--- commons/trunk/src/main/java/org/databene/commons/accessor/FeatureAccessor.java
+++ commons/trunk/src/main/java/org/databene/commons/accessor/FeatureAccessor.java
@@ -37,6 +37,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 import java.util.Map;
 
 /**
@@ -75,7 +76,8 @@ public class FeatureAccessor<C, V> implements Accessor<C, V> {
 
     // Accessor interface implementation -------------------------------------------------------------------------------
 
-	public V getValue(C target) {
+	@SuppressWarnings("unchecked")
+    public V getValue(C target) {
         return (V) getValue(target, featureName);
     }
 
@@ -87,15 +89,16 @@ public class FeatureAccessor<C, V> implements Accessor<C, V> {
         return getValue(target, featureName, true);
     }
 
+    @SuppressWarnings("unchecked")
     public static Object getValue(Object target, String featureName, boolean strict) {
         if (target == null)
             return null;
         else if (target instanceof Map)
-            return ((Map<String, Object>)target).get(featureName);
+            return ((Map<String, Object>) target).get(featureName);
         else if (target instanceof Context)
-            return ((Context)target).get(featureName);
+            return ((Context) target).get(featureName);
         else if (target instanceof Composite)
-            return ((Composite)target).getComponent(featureName);
+            return ((Composite) target).getComponent(featureName);
         else {
             PropertyDescriptor propertyDescriptor = BeanUtil.getPropertyDescriptor(target.getClass(), featureName);
             if (propertyDescriptor != null) {
@@ -104,6 +107,11 @@ public class FeatureAccessor<C, V> implements Accessor<C, V> {
                 } catch (Exception e) {
                     throw new ConfigurationError("Unable to access feature '" + featureName + "'", e);
                 }
+            } else {
+            	Class<?> type = ((target instanceof Class) ? (Class<?>) target : target.getClass());
+            	Field field = BeanUtil.getField(type, featureName);
+            	if (field != null)
+            		return BeanUtil.getFieldValue(field, target, false);
             }
         }
         // the feature has not been identified, yet - escalate or raise an exception
