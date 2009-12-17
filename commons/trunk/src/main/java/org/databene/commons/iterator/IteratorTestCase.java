9b574002f1c258e7fe1320662c7b6d6d01aad020
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-12-17 05:59:49 +0000

    Improved error messaging

--- commons/trunk/src/main/java/org/databene/commons/iterator/IteratorTestCase.java
+++ commons/trunk/src/main/java/org/databene/commons/iterator/IteratorTestCase.java
@@ -11,17 +11,18 @@ public abstract class IteratorTestCase {
     public static <T> void checkUniqueIteration(Iterator<T> iterator, int count) {
         Set<T> items = new HashSet<T>(count);
         for (int i = 0; i < count; i++) {
-            assertTrue(iterator.hasNext());
+            assertTrue("Iterator is expected to be available", iterator.hasNext());
             T item = iterator.next();
-            assertFalse(items.contains(item));
+            assertFalse("Item not unique: " + item, items.contains(item));
             items.add(item);
         }
     }
 
 	public static <T> NextHelper<T> expectNextElements(Iterator<T> iterator, T... elements) {
 		for (T element : elements) {
-			assertTrue(iterator.hasNext());
-			assertEquals(element, iterator.next());
+			assertTrue("Iterator is expected to be available", iterator.hasNext());
+			T next = iterator.next();
+			assertEquals("Expected " + element + " but was: " + next, element, next);
 		}
 		return new NextHelper<T>(iterator);
 	}
@@ -35,11 +36,11 @@ public abstract class IteratorTestCase {
 		}
 		
 		public void withNext() {
-			assertTrue(iterator.hasNext());
+			assertTrue("Iterator is expected to be still available", iterator.hasNext());
 		}
 		
 		public void withNoNext() {
-			assertFalse(iterator.hasNext());
+			assertFalse("Iterator is not expected to be available any more", iterator.hasNext());
 		}
 	}
 	
