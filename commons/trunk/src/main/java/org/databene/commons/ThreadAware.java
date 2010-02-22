615cbd2d0d32afa469ddfde788aa647a5d7f0c99
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-22 09:36:44 +0000

    Improved ThreadAware interface

--- commons/trunk/src/main/java/org/databene/commons/ThreadAware.java
+++ commons/trunk/src/main/java/org/databene/commons/ThreadAware.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2009-2010 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -28,5 +28,6 @@ package org.databene.commons;
  * @author Volker Bergmann
  */
 public interface ThreadAware {
-	ThreadSupport getThreading();
+	boolean isParallelizable();
+	boolean isThreadSafe();
 }
