9d34215ef7929f140fa49274bb9be2d832b82167
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-22 16:15:16 +0000

    Providing shell output to the caller

--- commons/trunk/src/main/java/org/databene/commons/ShellUtil.java
+++ commons/trunk/src/main/java/org/databene/commons/ShellUtil.java
@@ -29,7 +29,9 @@ package org.databene.commons;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Writer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,40 +47,25 @@ public class ShellUtil {
 	
 	private static Logger logger = LoggerFactory.getLogger(ShellUtil.class);
 
-	public static int runShellCommands(ReaderLineIterator iterator, ErrorHandler errorHandler) {
+	public static int runShellCommands(ReaderLineIterator iterator, Writer outputWriter, ErrorHandler errorHandler) {
 		int result = 0;
 		while (iterator.hasNext()) {
 			String command = iterator.next().trim();
 			if (command.length() > 0)
-				result = runShellCommand(command, errorHandler);	
+				result = runShellCommand(command, outputWriter, errorHandler);
 		}
 		return result;
     }
 
-	public static int runShellCommand(String command, ErrorHandler errorHandler) {
-		return runShellCommand(command, new File(SystemInfo.getCurrentDir()), errorHandler);
+	public static int runShellCommand(String command, Writer outputWriter, ErrorHandler errorHandler) {
+		return runShellCommand(command, outputWriter, new File(SystemInfo.getCurrentDir()), errorHandler);
 	}
 	
-	public static int runShellCommand(String command, File directory, ErrorHandler errorHandler) {
+	public static int runShellCommand(String command, Writer outputWriter, File directory, ErrorHandler errorHandler) {
 		logger.debug(command);
 		try {
-			String s = null;
-			Process p = Runtime.getRuntime().exec(command, null, directory);
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			BufferedReader stdErr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			// read the output from the command
-			while ((s = stdIn.readLine()) != null) {
-			    System.out.println(s);
-			}
-			// read any errors from the attempted command
-			while ((s = stdErr.readLine()) != null) {
-			    System.out.println(s);
-			}
-			p.waitFor();
-			int exitValue = p.exitValue();
-			if (exitValue != 0)
-				errorHandler.handleError("Process (" + command + ") did not terminate normally: Return code " + exitValue);
-			return exitValue;
+			Process process = Runtime.getRuntime().exec(command, null, directory);
+			return execute(process, command, outputWriter, errorHandler);
 		} catch (FileNotFoundException e) {
 			errorHandler.handleError("Error in shell invocation: " + command, e);
 			return 2;
@@ -88,33 +75,47 @@ public class ShellUtil {
 		}
 	}
 
-	public static void runShellCommand(String[] cmdArray, File directory, ErrorHandler errorHandler) {
+	public static int runShellCommand(String[] cmdArray, Writer outputWriter, File directory, ErrorHandler errorHandler) {
+		String description = renderCmdArray(cmdArray);
 		if (logger.isDebugEnabled())
-			logger.debug(renderCmdArray(cmdArray));
+			logger.debug(description);
 		try {
-			String s = null;
-			Process p = Runtime.getRuntime().exec(cmdArray, null, directory);
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			BufferedReader stdErr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			// read the output from the command
-			while ((s = stdIn.readLine()) != null) {
-			    System.out.println(s);
-			}
-			// read any errors from the attempted command
-			while ((s = stdErr.readLine()) != null) {
-			    System.out.println(s);
-			}
-			p.waitFor();
-			if (p.exitValue() != 0) {
-				String command = renderCmdArray(cmdArray);
-				errorHandler.handleError("Process (" + command + ") did not terminate normally: Return code " + p.exitValue());
-			}
+			Process process = Runtime.getRuntime().exec(cmdArray, null, directory);
+			return execute(process, description, outputWriter, errorHandler);
 		} catch (Exception e) {
-			String command = renderCmdArray(cmdArray);
-			errorHandler.handleError("Error in shell invocation: " + command, e);
+			errorHandler.handleError("Error in shell invocation: " + description, e);
+			return -1;
 		}
 	}
 	
+	private static int execute(Process process, String description, Writer outputWriter, ErrorHandler errorHandler)
+            throws IOException, InterruptedException {
+        String lf = SystemInfo.getLineSeparator();
+	    BufferedReader stdIn = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	    BufferedReader stdErr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+	    // read the output from the command
+	    boolean firstLine = true;
+	    String s;
+	    while ((s = stdIn.readLine()) != null) {
+	    	if (outputWriter != null) {
+		    	if (firstLine)
+		    		firstLine = false;
+	            else
+		            outputWriter.write(lf);
+		        outputWriter.write(s);
+	    	}	        
+	    }
+	    // read any errors from the attempted command
+	    while ((s = stdErr.readLine()) != null) {
+	        errorHandler.handleError(s);
+	    }
+	    process.waitFor();
+	    int exitValue = process.exitValue();
+	    if (exitValue != 0)
+	    	errorHandler.handleError("Process (" + description + ") did not terminate normally: Return code " + exitValue);
+	    return exitValue;
+    }
+
 	// private helpers ---------------------------------------------------------
 
 	private static String renderCmdArray(String[] cmdArray) {
