842cde0e3641acc69a129cc998d7a9c37ee2301f
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-03-12 15:56:55 +0000

    Adapted to code change

--- commons/trunk/src/test/java/org/databene/commons/xml/XMLUtilTest.java
+++ commons/trunk/src/test/java/org/databene/commons/xml/XMLUtilTest.java
@@ -148,7 +148,7 @@ public class XMLUtilTest {
             FileWriter writer = new FileWriter(file);
             writer.write(XML_TEXT);
             writer.close();
-            Document document = XMLUtil.parse(file.getAbsolutePath(), false);
+            Document document = XMLUtil.parse(file.getAbsolutePath(), null);
             checkXML(document);
         } finally {
             file.delete();
