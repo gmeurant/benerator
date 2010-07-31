1c1c74d1d03e754a3c7244f63fe47fe21d21cadc
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-07-31 09:51:08 +0000

    Added support for Mac OS X application adapter

---
+++ gui/trunk/src/main/java/org/databene/gui/os/mac/MacApplicationAdapter.java
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.gui.os.mac;
+
+import org.databene.gui.os.Application;
+
+import com.apple.eawt.ApplicationAdapter;
+import com.apple.eawt.ApplicationEvent;
+
+/**
+ * TODO Document class.<br/><br/>
+ * Created: 09.06.2010 13:56:54
+ * @since 0.2.3
+ * @author Volker Bergmann
+ */
+public class MacApplicationAdapter extends ApplicationAdapter {
+
+	private Application application;
+	
+	public MacApplicationAdapter(Application application) {
+	    this.application = application;
+    }
+
+	@Override
+    public void handleQuit(ApplicationEvent e) {
+		application.exit();
+	}
+
+	@Override
+    public void handleAbout(ApplicationEvent e) {
+		// tell the system we're handling this, so it won't display
+		// the default system "about" dialog after ours is shown.
+		e.setHandled(true);
+		application.about();
+	}
+
+}
